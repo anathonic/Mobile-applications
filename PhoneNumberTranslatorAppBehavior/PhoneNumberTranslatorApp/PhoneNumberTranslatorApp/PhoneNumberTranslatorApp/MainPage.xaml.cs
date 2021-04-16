c5f359de8207871e35d833ec8307f4297e875156
Anita Tabaczka <anitatabacka@gmail.com>
2021-04-16 21:37:44 +0200

    Exercise - Add behavior to your XAML page

---
+++ PhoneNumberTranslatorAppBehavior/PhoneNumberTranslatorApp/PhoneNumberTranslatorApp/PhoneNumberTranslatorApp/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneNumberTranslatorApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = Core.PhoneNumberTranslatorApp.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                 PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+    }
+}
---
+++ PhoneNumberTranslatorAppBehavior/PhoneNumberTranslatorApp/PhoneNumberTranslatorApp/PhoneNumberTranslatorApp/SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace PhoneNumberTranslatorApp
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0xa5, 0); }
+        }
+
+        public static Color ButtonTextColor
+        {
+            get { return Color.White; }
+        }
+    }
+}
