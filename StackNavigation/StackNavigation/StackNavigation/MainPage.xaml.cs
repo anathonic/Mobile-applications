ed5bfa84b376c3358ac42c750143dd187256646c
Anita Tabaczka <anitatabacka@gmail.com>
2021-06-02 16:08:38 +0200

    StackNavigation

---
+++ StackNavigation/StackNavigation/StackNavigation/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using StackNavigation.page;
+
+namespace StackNavigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAboutus_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AboutAss());
+        }
+    }
+}
---
+++ StackNavigation/StackNavigation/StackNavigation/page/AboutAss.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackNavigation.page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutAss : ContentPage
+    {
+        public AboutAss()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContact_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Contact(), animated: true); 
+        }
+    }
+}
